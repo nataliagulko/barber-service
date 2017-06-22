c5903cb69479086556c4565fc81e36b552aad866
Ногиева Наталья <n.nogieva@dodopizza.com>
2017-06-22 11:25:51 +0300

    Fix auth config params

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
-	authorizer: 'authorizer:token'
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+    authorizer: 'authorizer:token'
 });
