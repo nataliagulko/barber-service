c5ed00e175968f05558b67674b8e442482601d19
Наталья Гулько <n.gulko@dodopizza.com>
2017-08-11 16:54:22 +0300

    Вменяемая форма логина

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,17 +21,17 @@ module.exports = function(defaults) {
 
   // Plugins Fonts
   // Font Awesome
-  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.eot");
-  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.svg");
-  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.ttf");
-  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.woff");
-  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.woff");
+  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.eot", { destDir: 'fonts' });
+  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.svg", { destDir: 'fonts' });
+  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.ttf", { destDir: 'fonts' });
+  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.woff", { destDir: 'fonts' });
+  app.import("vendor/plugins/font-awesome/fonts/fontawesome-webfont.woff", { destDir: 'fonts' });
   // Glyphicons
-  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.eot");
-  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.svg");
-  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.ttf");
-  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.woff");
-  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.woff2");
+  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.eot", { destDir: 'fonts' });
+  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.svg", { destDir: 'fonts' });
+  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.ttf", { destDir: 'fonts' });
+  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.woff", { destDir: 'fonts' });
+  app.import("vendor/plugins/bootstrap/fonts/bootstrap/glyphicons-halflings-regular.woff2", { destDir: 'fonts' });
 
   // Plugins JS
   app.import("vendor/plugins/jquery-ui/jquery-ui.min.js");
@@ -42,11 +42,19 @@ module.exports = function(defaults) {
   app.import("vendor/plugins/moment.min.js");
   app.import("vendor/plugins/bootstrap-switch/js/bootstrap-switch.js");
   app.import("vendor/plugins/fullcalendar/fullcalendar.js");
+  app.import("vendor/plugins/jquery-validation/js/jquery.validate.js");
+  app.import("vendor/plugins/jquery-validation/js/localization/messages_ru.js");
+  app.import("vendor/plugins/backstretch/jquery.backstretch.js");
 
-  // Page JS
+  // Layout JS
   app.import("vendor/scripts/app.js");
   app.import("vendor/scripts/layout/layout.js");
   app.import("vendor/scripts/quick-sidebar.js");
+
+  // Pages JS
+  // Login 4
+  app.import("vendor/pages/login/login-4.js");
+  app.import("vendor/pages/login/login-4.css");
 
   // If you need to use different assets in different
   // environments, specify an object as the first parameter. That
