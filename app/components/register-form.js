60ab7326ca4e220e45feb776f40e3cdf401b16f6
Наталья Гулько <n.gulko@dodopizza.com>
2017-08-14 16:32:22 +0300

    Пример запроса для регистрации

--- app/components/register-form.js
+++ app/components/register-form.js
@@ -1,16 +1,25 @@
 import Ember from 'ember';
+import config from 'barbers/config/environment';
 
 export default Ember.Component.extend({
 	classNames: ["register-form"],
+	session: Ember.inject.service(),
 
 	actions: {
 		showLoginForm: function() {
-        	$('.register-form').hide();
-        	$('#login-form').show();
-        },
+			$('.register-form').hide();
+			$('#login-form').show();
+		},
 
-        register: function() {
-        	console.log('register');
-        }
+		register: function() {
+			var params = $('.register-form form').serialize();
+
+			$.post({
+				url: config.host + '/UserAjax/create',
+				data: params
+			}).then((response) => {
+				this.send('showLoginForm');
+			});
+		}
 	}
 });
