715ec9b2fd70debfea9ed94b0b63723138c99f04
Viktor Tyupikin <misterrr2010@gmail.com>
2017-08-20 13:02:50 +0300

    Добавил футер, привел в порядок общую структуру страницы, заменил функцию size() на length

--- client/app/components/page-header.js
+++ client/app/components/page-header.js
@@ -4,7 +4,7 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Ember.Component.extend(ApplicationRouteMixin, {
 	session: Ember.inject.service('session'),
 	classNames: ['page-header', 'navbar', 'navbar-fixed-top'],
-	
+
     actions: {
         invalidateSession: function() {
             this.get('session').invalidate();
