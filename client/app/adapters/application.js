b3046247ca6bce0e68b01b03ff17a1c5d62f1d0c
Наталья Гулько <n.gulko@dodopizza.com>
2017-09-11 16:31:09 +0300

    Тестирование модели на услугах

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,12 +1,29 @@
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import config from './config/environment';
+import config from '../config/environment';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
-    namespace: 'api',
-    authorizer: 'authorizer:token',
-    host: config.host,
-    headers: {
-        withCredentials: true
-    }
+	namespace: 'api',
+	authorizer: 'authorizer:token',
+	host: config.host,
+	headers: {
+		withCredentials: true
+	},
+	pathForType: function(type) {
+		var camelized = Ember.String.camelize(type);
+		return Ember.String.singularize(camelized);
+	},
+	findAll: function(store, type, sinceToken) {
+		var query, url;
+
+		if (sinceToken) {
+			query = { since: sinceToken };
+		}
+
+		url = this.buildURL(type.modelName, null, null, 'findAll');
+
+		return this.ajax(url + "Ajax/list", 'POST', {
+			data: query
+		});
+	},
 });
