2f20419b1d805c62777a006500f5a2df6e023104
Наталья Гулько <n.gulko@dodopizza.com>
2017-09-12 17:57:42 +0300

    Смена пароля с подтверждением по коду

--- client/app/components/forget-form.js
+++ client/app/components/forget-form.js
@@ -1,14 +1,27 @@
 import Ember from 'ember';
+import config from 'barbers/config/environment';
 
 export default Ember.Component.extend({
 	classNames: ['forget-form'],
 	validate: Ember.inject.service(),
+	isCodeSent: false,
 
 	didInsertElement: function() {
 		var validate = this.get('validate'),
 			options = {
 				rules: {
-					phone: 'required'
+					phone: 'required',
+					pass: {
+						required: true,
+						minlength: 6,
+						maxlength: 20
+					},
+					rpass: {
+						required: true,
+						minlength: 6,
+						maxlength: 20,
+						equalTo: "[name=pass]"
+					}
 				}
 			};
 
@@ -21,8 +34,39 @@ export default Ember.Component.extend({
 			$('#login-form').show();
 		},
 
-		forget: function() {
-			console.log('forget');
+		sumbitCode: function() {
+			var params = $("#forget-form").serialize();
+			this.set('isCodeSent', true);
+
+			$.post({
+				url: config.host + '/register/createChangePassRequest',
+				data: params
+			}).then((response) => {
+
+				if (!response.error) {
+					$("#requestId").val(response.id);
+				} else {
+					toast.error(response.error, '', options);
+				}
+			});
+		},
+
+		checkCode: function() {
+			var params = $("#forget-form").serialize();
+			console.log(params);
+
+			$.post({
+				url: config.host + '/register/submitChangePassRequest',
+				data: params
+			}).then((response) => {
+
+				if (!response.error) {
+					$("#forget-form")[0].reset();
+					this.send('showLoginForm');
+				} else {
+					toast.error(response.error, '', options);
+				}
+			});
 		}
 	}
 });
