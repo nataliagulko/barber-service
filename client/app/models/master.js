787ca42f8d8c29d8b61e96aa7b0bd68102225891
Наталья Гулько <n.gulko@dodopizza.com>
2017-09-29 09:34:58 +0300

    Создаем мастера

--- client/app/models/master.js
+++ client/app/models/master.js
@@ -3,9 +3,11 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     username: DS.attr(),
     password: DS.attr(),
+    rpassword: DS.attr(),
     email: DS.attr(),
     firstname: DS.attr(),
     secondname: DS.attr(),
+    phone: DS.attr(),
     masterTZ: DS.attr(),
     enabled: DS.attr(),
     accountExpired: DS.attr(),
--- client/app/routes/master/create.js
+++ client/app/routes/master/create.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+	model() {
+		// return this.store.createRecord('service', {
+		// 	masters: this.get('store').findAll('master')
+		// });
+		return this.store.createRecord('master');
+	},
 });
