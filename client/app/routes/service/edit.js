52de37ad36e378b426c392a75b2ae87165d310b8
Natalya Gulko <n.gulko@dodopizza.com>
2017-12-01 12:16:58 +0300

    edit services: first steps

--- client/app/routes/service/edit.js
+++ client/app/routes/service/edit.js
@@ -5,9 +5,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
 		model(params) {
 		return Ember.RSVP.hash({
 			service: this.get('store').findRecord('service', params.id),
-			serviceGroup: this.get('store').createRecord('serviceGroup'),
-			masters: this.get('store').findAll('master'),
-			subservices: this.get('store').findAll('service'),
+			masters: this.get('store').findAll('master')
 		});
 	},
 });
