64b72b7b888c441eff7dc0143e830ea45f8284d0
Natalya Gulko <n.gulko@dodopizza.com>
2018-03-16 17:15:01 +0300

    colors for ticket statuses

--- client/app/components/ticket-list/calendar-filters.js
+++ client/app/components/ticket-list/calendar-filters.js
@@ -1,6 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    ticketStatuses: [
+        {
+            alias: "new",
+            text: "Новые",
+            fontClass: "font-blue-sharp",
+            bgClasses: ["bg-blue-sharp", "bg-font-blue-sharp"]
+        },
+        {
+            alias: "accepted",
+            text: "Подтвержденные",
+            fontClass: "font-green-turquoise",
+            bgClasses: ["bg-green-turquoise", "bg-font-green-turquoise"]
+        },
+        {
+            alias: "rejected",
+            text: "Отклоненные",
+            fontClass: "font-red-pink",
+            bgClasses: ["bg-red-pink", "bg-font-red-pink"]
+        },
+        {
+            alias: "canceled",
+            text: "Отмененные",
+            fontClass: "font-yellow-lemon",
+            bgClasses: ["bg-yellow-lemon", "bg-font-yellow-lemon"]
+        },
+        {
+            alias: "completed",
+            text: "Завершенные",
+            fontClass: "font-default",
+            bgClasses: ["bg-default", "bg-font-default"]
+        }
+    ],
+
     actions: {
         filterEventsByStatus: function (status) {
             const $calendar = Ember.$(".full-calendar"),
