59208d6364877c46ffe42af44e26c04ad12e5dff
ngulko <n.gulko@dodopizza.com>
2018-03-22 17:05:02 +0300

    show timepicker for day of week

--- client/app/components/master/master-schedule.js
+++ client/app/components/master/master-schedule.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
+import moment from 'moment';
 import _ from 'lodash';
 
 export default Ember.Component.extend({
+    classNames: ["master-schedule"],
     store: Ember.inject.service("store"),
+    pickatimeService: Ember.inject.service("pickatime-service"),
     groupedWorkTimes: [],
 
     didInsertElement() {
@@ -18,5 +21,25 @@ export default Ember.Component.extend({
             let groupedWorkTimes = _.groupBy(workTimes.toArray(), "data.dayOfWeek");
             this.set("groupedWorkTimes", groupedWorkTimes);
         });
+    },
+
+    actions: {
+        onTimeButtonClick: function (workTime) {
+            const pickatimeService = this.get("pickatimeService"),
+                timeFrom = workTime.get("timeFrom").split(":"),
+                timeTo = workTime.get("timeTo").split(":");
+
+            let pickerSelector = "#master-schedule__time-picker",
+                rangeArr = [{
+                    "from": [timeFrom[0], timeFrom[1]],
+                    "to": [timeTo[0], timeTo[1]]
+                }];
+
+            pickatimeService.set(pickerSelector, "disable", false);
+            pickatimeService.set(pickerSelector, "interval", 30);
+            pickatimeService.set(pickerSelector, "min", [8, 0]);
+            pickatimeService.set(pickerSelector, "max", [20, 0]);
+            pickatimeService.set(pickerSelector, "disable", rangeArr);
+        }
     }
 });
--- client/app/models/work-time.js
+++ client/app/models/work-time.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 	timeFrom: DS.attr(),
@@ -6,5 +7,8 @@ export default DS.Model.extend({
 	dayOfWeek: DS.attr(),
 	dateFrom: DS.attr(),
 	dateTo: DS.attr(),
-	master: DS.belongsTo('master')
+	master: DS.belongsTo('master'),
+	timeRange: Ember.computed('timeFrom', 'timeTo', function() {
+        return `${this.get('timeFrom')}—${this.get('timeTo')}`;
+    })
 });
