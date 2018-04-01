43d75027ebf6b3d176eccb3de57d42b8d2ebbe23
Viktor Tyupikin <misterrr2010@gmail.com>
2018-04-01 22:59:13 +0300

    рефакторинг

--- client/app/components/ticket-phone-remove-btn.js
+++ client/app/components/ticket-phone-remove-btn.js
@@ -10,25 +10,4 @@ export default Ember.Component.extend({
             ticketService.removeLastNumber();
         }
     }
-
-    // actions: {
-    //     removeLastNumber() {
-    //         let phonenumber = this.get("phone");
-    //         phonenumber.slice(0,-1);
-
-    //         this.set("phone", phonenumber);
-
-
-
-    //         // var inputField = $('.ticket-info-client-top__input'),
-    //         //     inputValue = $('#phone-number'),
-    //         //     bottomInput = $('.ticket-info-client__phone'),
-    //         //     phoneValue = $(inputValue).val();
-
-    //         //     phoneValue = phoneValue.slice(0,-1);
-    //         //     $(inputValue).val(phoneValue);
-    //         //     $(inputField).text(phoneValue);
-    //         //     $(bottomInput).text(phoneValue);
-    //     }
-    // }
 });
