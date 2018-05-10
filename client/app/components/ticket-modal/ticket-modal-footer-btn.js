4bb000053c7d1ba4e7d7ba38df7d15e441de09b7
Natalya Gulko <n.gulko@dodopizza.com>
2018-05-10 16:27:44 +0300

    update ticket status

--- client/app/components/ticket-modal/ticket-modal-footer-btn.js
+++ client/app/components/ticket-modal/ticket-modal-footer-btn.js
@@ -4,29 +4,43 @@ export default Ember.Component.extend({
     tagName: "button",
     classNames: ["btn"],
     store: Ember.inject.service("store"),
-	bootbox: Ember.inject.service("bootbox-service"),
+    bootbox: Ember.inject.service("bootbox-service"),
 
-    click(e) {
+    click() {
         const act = this.get("act"),
             event = this.get("event");
 
         this.send(act, event);
     },
 
+    updateTicketStatus: function(event, status) {
+        const store = this.get("store");
+
+        store.findRecord("ticket", event.id)
+            .then((ticket) => {
+                ticket.set("status", status);
+                ticket
+                    .save()
+                    .then((ticket) => {
+                        console.log(ticket.get("status"));
+                    });
+            });
+    },
+
     actions: {
         remove: function (event) {
             const store = this.get("store");
-            
-            Ember.$("#ticket-modal").modal('hide');                        
+
+            Ember.$("#ticket-modal").modal('hide');
             this.get("bootbox").confirmDelete(store, "ticket", event.id, "запись");
         },
 
         reject: function (event) {
-            console.log("reject", event);
+            this.updateTicketStatus(event, "REJECTED");
         },
 
         accept: function (event) {
-            console.log("accept", event);
+            this.updateTicketStatus(event, "ACCEPTED");
         }
     }
 });
