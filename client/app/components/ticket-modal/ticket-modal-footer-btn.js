241bfaec7c9e386154713cfe0a5524027d3a7d67
Natalya Gulko <n.gulko@dodopizza.com>
2018-05-10 15:55:40 +0300

    working btn for ticket destroy, but not working method on server

--- client/app/components/ticket-modal/ticket-modal-footer-btn.js
+++ client/app/components/ticket-modal/ticket-modal-footer-btn.js
@@ -3,14 +3,30 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName: "button",
     classNames: ["btn"],
+    store: Ember.inject.service("store"),
+	bootbox: Ember.inject.service("bootbox-service"),
 
     click(e) {
-        console.log("click", this.get("event"));
+        const act = this.get("act"),
+            event = this.get("event");
+
+        this.send(act, event);
     },
 
     actions: {
-        submitRemove: () => {
-            console.log("submitRemove");
+        remove: function (event) {
+            const store = this.get("store");
+            
+            Ember.$("#ticket-modal").modal('hide');                        
+            this.get("bootbox").confirmDelete(store, "ticket", event.id, "запись");
+        },
+
+        reject: function (event) {
+            console.log("reject", event);
+        },
+
+        accept: function (event) {
+            console.log("accept", event);
         }
     }
 });
