a1e229766f1447ed264ae1117c4616058a297506
Natalya Gulko <n.gulko@dodopizza.com>
2018-05-10 14:45:19 +0300

    removed ticket-list/ticket-info, add ticket-modal/

--- client/app/components/ticket-list/calendar-filters.js
+++ client/app/components/ticket-list/calendar-filters.js
@@ -6,31 +6,36 @@ export default Ember.Component.extend({
             alias: "new",
             text: "Новые",
             fontClass: "font-blue-sharp",
-            bgClasses: ["bg-blue-sharp", "bg-font-blue-sharp"]
+            bgClasses: ["bg-blue-sharp", "bg-font-blue-sharp"],
+            btnClass: "blue-sharp"
         },
         {
             alias: "accepted",
             text: "Подтвержденные",
             fontClass: "font-green-turquoise",
-            bgClasses: ["bg-green-turquoise", "bg-font-green-turquoise"]
+            bgClasses: ["bg-green-turquoise", "bg-font-green-turquoise"],
+            btnClass: "green-turquoise"
         },
         {
             alias: "rejected",
             text: "Отклоненные",
             fontClass: "font-red-pink",
-            bgClasses: ["bg-red-pink", "bg-font-red-pink"]
+            bgClasses: ["bg-red-pink", "bg-font-red-pink"],
+            btnClass: "red-pink"
         },
         {
             alias: "canceled",
             text: "Отмененные",
             fontClass: "font-yellow-lemon",
-            bgClasses: ["bg-yellow-lemon", "bg-font-yellow-lemon"]
+            bgClasses: ["bg-yellow-lemon", "bg-font-yellow-lemon"],
+            btnClass: "yellow-lemon"
         },
         {
             alias: "completed",
             text: "Завершенные",
             fontClass: "font-default",
-            bgClasses: ["bg-default", "bg-font-default"]
+            bgClasses: ["bg-default", "bg-font-default"],
+            btnClass: "btn-default"
         }
     ],
 
--- client/tests/integration/components/ticket-modal/ticket-modal-test.js
+++ client/tests/integration/components/ticket-modal/ticket-modal-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ticket-list/ticket-info', 'Integration | Component | ticket list/ticket info', {
+moduleForComponent('ticket-modal/ticket-modal', 'Integration | Component | ticket modal/ticket modal', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{ticket-list/ticket-info}}`);
+  this.render(hbs`{{ticket-modal/ticket-modal}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#ticket-list/ticket-info}}
+    {{#ticket-modal/ticket-modal}}
       template block text
-    {{/ticket-list/ticket-info}}
+    {{/ticket-modal/ticket-modal}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
