89ae0ebb1b9b64a67fba03365929366b9fa22708
ngulko <n.gulko@dodopizza.com>
2018-06-14 07:38:10 +0300

    remove work-time components

--- client/app/components/master-schedule/master-schedule.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-    classNames: ["master-schedule"],
-    store: Ember.inject.service("store"),
-    selectedDayOfWeek: null,
-});
-
