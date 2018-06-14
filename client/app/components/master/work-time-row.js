4db94faf44e26d8a5b5c4f4c2913beb7e489d670
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-14 12:00:38 +0300

    show work times by day of week and weekends

--- client/app/components/master/work-time-row.js
+++ client/app/components/master/work-time-row.js
@@ -1,4 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    tagName: 'form',
+    classNames: ['form-inline']
 });
--- client/app/components/master/work-times.js
+++ client/app/components/master/work-times.js
@@ -4,19 +4,57 @@ import _ from 'lodash';
 export default Component.extend({
     didInsertElement() {
         const workTimes = this.get("workTimes").toArray();
+        let modifiedWorkTimes = [];
+        
+        const workingDays = this.buildWordingDays(workTimes);
+        const weekend = this.buildWeekend(workTimes);
+        modifiedWorkTimes = workingDays.concat(weekend);
+        
+        modifiedWorkTimes = _.sortBy(modifiedWorkTimes, "dayOfWeek");
+        this.set("modifiedWorkTimes", modifiedWorkTimes);
+    },
+    
+    buildWordingDays: function (workTimes) {
+        const times = [];
         const workTimesByDayOfWeek = _.groupBy(workTimes, "data.dayOfWeek");
-        const modifiedWorkTimes = [];
 
         _.forEach(workTimesByDayOfWeek, function (workTime) {
-            // const dayOfWeek = workTime[0].get("dayOfWeek");
-            const start = workTime[0].get("timeFrom");
-            const lunchStart = workTime[0].get("timeTo");
-            const lunchEnd = workTime[1].get("timeFrom");
-            const end = workTime[1].get("timeTo");
-            modifiedWorkTimes.push({start, end, lunchStart, lunchEnd});
+            let item;
+
+            if (workTime.length > 1) {
+                const dayOfWeek = workTime[0].get("dayOfWeek");
+                const start = workTime[0].get("timeFrom");
+                const lunchStart = workTime[0].get("timeTo");
+                const lunchEnd = workTime[1].get("timeFrom");
+                const end = workTime[1].get("timeTo");
+                item = { dayOfWeek, start, end, lunchStart, lunchEnd, checked: true };
+            } else {
+                const dayOfWeek = workTime[0].get("dayOfWeek");
+                const start = workTime[0].get("timeFrom");
+                const end = workTime[0].get("timeTo");
+                const lunchStart = null;
+                const lunchEnd = null;
+                item = { dayOfWeek, start, end, lunchStart, lunchEnd, checked: true };
+            }
+
+            times.push(item);
         });
-        
-        // console.log(modifiedWorkTimes);
-        this.set("modifiedWorkTimes", modifiedWorkTimes);
+
+        return times;
+    },
+
+    buildWeekend: function (workTimes) {
+        let daysOfWeek = [0, 1, 2, 3, 4, 5, 6];
+        const times = [];
+
+        daysOfWeek.forEach((day) => {
+            let dayOfWeek = _.find(workTimes, _.iteratee(['data.dayOfWeek', day]));
+
+            if (typeof dayOfWeek === "undefined") {
+                times.push({  dayOfWeek: day, start: null, end: null, lunchStart: null, lunchEnd: null, checked: false });
+            }
+        });
+
+        return times;
     }
 });
