98d2fb4a8a8476b32c8d79228f06bbaea7035e1c
ngulko <n.gulko@dodopizza.com>
2018-06-14 08:36:52 +0300

    get start/end of day

--- client/app/components/master/work-times.js
+++ client/app/components/master/work-times.js
@@ -1,11 +1,22 @@
 import Component from '@ember/component';
 import _ from 'lodash';
 
-export default Component.extend({    
+export default Component.extend({
     didInsertElement() {
         const workTimes = this.get("workTimes").toArray();
         const workTimesByDayOfWeek = _.groupBy(workTimes, "data.dayOfWeek");
-        console.log(workTimesByDayOfWeek);
-        this.set("workTimesByDayOfWeek", workTimesByDayOfWeek);
+        const modifiedWorkTimes = [];
+
+        _.forEach(workTimesByDayOfWeek, function (workTime) {
+            // const dayOfWeek = workTime[0].get("dayOfWeek");
+            const start = workTime[0].get("timeFrom");
+            const lunchStart = workTime[0].get("timeTo");
+            const lunchEnd = workTime[1].get("timeFrom");
+            const end = workTime[1].get("timeTo");
+            modifiedWorkTimes.push({start, end, lunchStart, lunchEnd});
+        });
+        
+        // console.log(modifiedWorkTimes);
+        this.set("modifiedWorkTimes", modifiedWorkTimes);
     }
 });
