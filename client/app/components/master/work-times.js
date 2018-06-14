61f7e115f91864ede79347eee2af3c1fefd71ae0
ngulko <n.gulko@dodopizza.com>
2018-06-14 08:28:48 +0300

    show work-times by day

--- client/app/components/master/work-times.js
+++ client/app/components/master/work-times.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
+import _ from 'lodash';
 
-export default Component.extend({
+export default Component.extend({    
+    didInsertElement() {
+        const workTimes = this.get("workTimes").toArray();
+        const workTimesByDayOfWeek = _.groupBy(workTimes, "data.dayOfWeek");
+        console.log(workTimesByDayOfWeek);
+        this.set("workTimesByDayOfWeek", workTimesByDayOfWeek);
+    }
 });
