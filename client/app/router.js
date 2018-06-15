64a88642915beb5acc49273379c352455f51464e
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-15 14:57:56 +0300

    move code to new routes, remove old routes

--- client/app/router.js
+++ client/app/router.js
@@ -7,26 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function () {
-  this.route('ticket', function () {
-    this.route('create');
-  });
-  this.route('service', function () {
-    this.route('create');
-    this.route('edit', { path: '/edit/:id' });
-  });
-  this.route('master', function () {
-    this.route('create');
-    this.route('edit', { path: '/edit/:master_id' });
-    this.route('work-time', { path: '/:master_id/work-time' });
-  });
   this.route('not-found', { path: '/*path' });
-  this.route('service-group', function () {
-    this.route('edit', { path: '/edit/:id' });
-  });
-
-  this.route('business', function () {
-    this.route('create');
-  });
   this.route('login', function () {
     this.route('forget');
   });
