154fd2f1e5ccb8cd2fa8c44435526ba8504573a3
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-15 17:18:16 +0300

    try to redirect to index after login

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+	routeAfterAuthentication: "authenticated",
 	session: Ember.inject.service('session'),
 
 	init() {
