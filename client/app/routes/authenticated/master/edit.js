25db5c9c36ebcab8867bea7bdd7cfc308e9e5ccf
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-15 15:19:19 +0300

    исправила руты на новые, добавила логику в руты

--- client/app/routes/authenticated/master/edit.js
+++ client/app/routes/authenticated/master/edit.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
+import RollbackAttributesMixin from 'barbers/mixins/rollback-attributes-mixin';
 
-export default Route.extend({
+export default Route.extend(RollbackAttributesMixin, {
+	model(params) {
+		return this.store.findRecord('master', params.master_id);
+	},
+
+	deactivate() {
+		this._super(...arguments);
+
+		const model = this.modelFor(this.routeName);
+		this.rollback(model);
+	}
 });
--- client/app/routes/authenticated/service-group/edit.js
+++ client/app/routes/authenticated/service-group/edit.js
@@ -1,4 +1,20 @@
 import Route from '@ember/routing/route';
+import RollbackAttributesMixin from 'barbers/mixins/rollback-attributes-mixin';
+import { hash } from 'rsvp';
 
 export default Route.extend({
+    model(params) {
+        return hash(RollbackAttributesMixin, {
+            serviceGroup: this.get('store').findRecord('service-group', params.id),
+            masters: this.get('store').findAll('master'),
+            subservices: this.get('store').findAll('service'),
+        });
+    },
+
+    deactivate() {
+        this._super(...arguments);
+
+        const model = this.modelFor(this.routeName);
+        this.rollback(model.serviceGroup);
+    }
 });
