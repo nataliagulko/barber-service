c591362c2ec63abfdc0d0a7ca77bc259266fa9b6
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-15 17:07:36 +0300

    поправила оставшиеся руты, показываю форму логина

--- client/app/routes/authenticated/service/create.js
+++ client/app/routes/authenticated/service/create.js
@@ -1,4 +1,28 @@
 import Route from '@ember/routing/route';
+import RollbackAttributesMixin from 'barbers/mixins/rollback-attributes-mixin';
+import { hash } from 'rsvp';
 
-export default Route.extend({
+export default Route.extend(RollbackAttributesMixin, {
+    model() {
+        const store = this.get('store');
+
+        return hash({
+            service: store.createRecord('service'),
+            serviceGroup: store.createRecord('serviceGroup'),
+            masters: store.findAll('master'),
+            subservices: store.query('service', {
+                query: {
+                    onlySimpleService: true
+                }
+            }),
+        });
+    },
+
+    deactivate() {
+        this._super(...arguments);
+
+        const model = this.modelFor(this.routeName);
+        this.rollback(model.service);
+        this.rollback(model.serviceGroup);
+    }
 });
--- client/app/routes/authenticated/service/edit.js
+++ client/app/routes/authenticated/service/edit.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
+import RollbackAttributesMixin from 'barbers/mixins/rollback-attributes-mixin';
+import { hash } from 'rsvp';
 
-export default Route.extend({
+export default Route.extend(RollbackAttributesMixin, {
+    model(params) {
+		return hash({
+			service: this.get('store').findRecord('service', params.id),
+			masters: this.get('store').findAll('master')
+		});
+	},
+
+	deactivate() {
+		this._super(...arguments);
+
+		const model = this.modelFor(this.routeName);
+		this.rollback(model.service);
+	}
 });
