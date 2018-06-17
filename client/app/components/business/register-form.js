9c2cbfa8dd0a57dd30a67ca6488668912d9a731c
ngulko <n.gulko@dodopizza.com>
2018-06-17 21:03:25 +0300

    registration

--- client/app/components/business/register-form.js
+++ client/app/components/business/register-form.js
@@ -3,6 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
     tabName: 'form',
     classNames: ['register-form'],
+    phoneMask: ['+', '7', '(', /[1-9]/, /\d/, /\d/, ')', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/],
 
     actions: {
         saveBusinessAndUser: function () {
