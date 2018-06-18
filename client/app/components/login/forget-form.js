28526dd54e89b6ef324d4a96839260537911ad61
Natalya Gulko <n.gulko@dodopizza.com>
2018-06-18 17:34:59 +0300

    add validation to forget-form

--- client/app/components/login/forget-form.js
+++ client/app/components/login/forget-form.js
@@ -1,13 +1,22 @@
 import $ from 'jquery';
 import Component from '@ember/component';
 import { inject } from '@ember/service';
+import { alias } from '@ember/object/computed';
 import config from 'barbers/config/environment';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default Component.extend({
+const Validations = buildValidations({
+	'phone': validator('presence', true),
+	'pass': validator('presence', true),
+	'rpass': validator('presence', true)
+});
+
+export default Component.extend(Validations, {
 	tagName: 'form',
 	classNames: ['forget-form'],
 	notification: inject("notification-service"),
 	phoneMask: ['+', '7', '(', /[1-9]/, /\d/, /\d/, ')', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/],
+	isFormInvalid: alias('validations.isInvalid'),
 	isCodeSent: false,
 
 	actions: {
@@ -27,7 +36,7 @@ export default Component.extend({
 			}).then((response) => {
 
 				if (!response.error) {
-					this.set("code", response.id);
+					this.set("requestId", response.id);
 					notification.showInfoMessage(response.code);
 				} else {
 					notification.showErrorMessage(response.error);
