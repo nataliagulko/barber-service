3285574b2ae4718526d9d697ab6d2e183ae6843a
Natalya Gulko <n.gulko@dodopizza.com>
2018-07-10 09:31:14 +0300

    set time mask to work-time

--- client/app/components/master/work-time-row.js
+++ client/app/components/master/work-time-row.js
@@ -1,7 +1,11 @@
 import Component from '@ember/component';
+import {inject} from '@ember/service';
+import { readOnly } from '@ember/object/computed';
 
 export default Component.extend({
     classNames: ['form-inline'],
+	constants: inject("constants-service"),
+	timeMask: readOnly("constants.TIME_MASK"),
 
     change() {
         const item = this.get("workTime");
--- client/app/services/constants-service.js
+++ client/app/services/constants-service.js
@@ -3,10 +3,12 @@ import Service from '@ember/service';
 export default Service.extend({
 	PHONE_MASK: null,
 	PHONE_LENGTH: 16,
+	TIME_MASK: null,
 
     init() {
         this._super(...arguments);
 
         this.set("PHONE_MASK", ['+', '7', '(', /[1-9]/, /\d/, /\d/, ')', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/]);
+        this.set("TIME_MASK", [/\d/, /\d/, ':', /\d/, /\d/]);
     }
 });
