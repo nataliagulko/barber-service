aec2bad9615a341ae4e1265a0eb84ebf6cd82a39
Natalya Gulko <n.gulko@dodopizza.com>
2018-07-12 12:33:52 +0300

    add, remove holidays

--- client/app/routes/auth/master/work-time.js
+++ client/app/routes/auth/master/work-time.js
@@ -2,6 +2,10 @@ import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 export default Route.extend({
+	queryParams: {
+		refreshModel: true
+	},
+
 	model(params) {
 		return hash({
 			workTimes: this.get("store").query("workTime", {
