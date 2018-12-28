c0567a6ff228897e3ee9c153d7a6a4b590aa0ab7
Natalya Gulko <n.gulko@dodopizza.com>
2018-12-28 15:48:26 +0300

    remove member-access from constants-service

--- client/app/services/constants-service.ts
+++ client/app/services/constants-service.ts
@@ -1,11 +1,11 @@
 import Service from "@ember/service";
 
 export default class ConstantsService extends Service {
-	public PHONE_MASK: Array<RegExp | string> = ["+", "7", "(", /[1-9]/, /\d/, /\d/, ")", /\d/, /\d/, /\d/, "-", /\d/, /\d/, "-", /\d/, /\d/];
-	public PHONE_LENGTH: number = 16;
-	public TIME_MASK: Array<RegExp | string> = [/\d/, /\d/, ":", /\d/, /\d/];
-	public DATE_MASK: Array<RegExp | string> = [/\d/, /\d/, ".", /\d/, /\d/, ".", /\d/, /\d/, /\d/, /\d/];
-	public DEFUALT_DATE_FORMAT: string = "DD.MM.YYYY";
+	PHONE_MASK: Array<RegExp | string> = ["+", "7", "(", /[1-9]/, /\d/, /\d/, ")", /\d/, /\d/, /\d/, "-", /\d/, /\d/, "-", /\d/, /\d/];
+	PHONE_LENGTH: number = 16;
+	TIME_MASK: Array<RegExp | string> = [/\d/, /\d/, ":", /\d/, /\d/];
+	DATE_MASK: Array<RegExp | string> = [/\d/, /\d/, ".", /\d/, /\d/, ".", /\d/, /\d/, /\d/, /\d/];
+	DEFUALT_DATE_FORMAT: string = "DD.MM.YYYY";
 }
 
 declare module "@ember/service" {
